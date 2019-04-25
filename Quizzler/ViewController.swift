f7e486de395a384d625ccbaf8365487f041bd3a8
Jodi Smit <jodi@nebulahome.com>
2019-04-25 14:56:23 -0400

    Added QuestionBank

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class ViewController: UIViewController {
     
     //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+	var pickedAnswer: Bool = false
+	var questionNumber = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,12 +22,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+		
+		let firstQuestion = allQuestions.list[0]
+		questionLabel.text = firstQuestion.questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+		if sender.tag == 1 {
+			pickedAnswer = true
+		} else {
+			pickedAnswer = false
+		}
+		
+		checkAnswer()
+		questionNumber += 1
+		
+		nextQuestion()
     }
     
     
@@ -35,12 +48,25 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+		
+		if questionNumber >= 12 {
+			questionLabel.text = allQuestions.list[questionNumber].questionText
+		} else {
+			print("End of quiz!")
+			questionNumber = 0
+		}
+		
     }
     
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+		
+		if correctAnswer == pickedAnswer {
+			print("Correct!")
+		} else {
+			print("Nope, try again")
+		}
     }
     
     
